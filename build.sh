7b53d549a674e22d88e67a923f516bd2ceffa6c8
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-09-23 22:40:31 +0800

    build script

--- build.sh
+++ build.sh
@@ -1,5 +1,48 @@
 #!/bin/bash
+helptext="
+$0
 
-racket make-page-tree.rkt
-raco pollen render index.ptree
-#raco pollen publish ./ ../build
+usage:
+  $0 build
+  $0 publish orig dest
+  $0 cleanup
+"
+
+build () {
+    racket make-page-tree.rkt
+    raco pollen render index.ptree
+}
+
+publish () {
+    # $1: origin (likely ./)
+    # $2: destination
+
+    # return if not supplied the arguments
+    [ -z "$1" ] && return
+    [ -z "$2" ] && return
+
+    build
+    raco pollen publish "$1" "$2"
+}
+
+cleanup () {
+    rm "*.html" "category/*.html" "category/*.html.pm"
+    raco pollen reset
+}
+
+case "$1" in
+    (build)
+        echo building
+        build
+        ;;
+    (publish)
+        echo publishing
+        publish "$2" "$3"
+        ;;
+    (clean|cleanup)
+        echo cleaning up
+        ;;
+    (*)
+        echo "$helptext"
+        ;;
+esac
