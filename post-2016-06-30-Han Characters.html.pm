d13980f3bbcd45928f659a51e82ae0065885f121
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-09-24 17:25:20 +0800

    New tags for 20160630 & language meta

--- post-2016-06-30-Han Characters.html.pm
+++ post-2016-06-30-Han Characters.html.pm
@@ -1,7 +1,8 @@
 #lang pollen
 ◊headline{Han Characters}
+◊define-meta[language]{en}
 ◊define-meta[publish-date]{2016/06/30 00:00}
-◊define-meta[categories]{DRAFT}
+◊define-meta[categories]{Mandarin, Han Characters, Language}
 
 If you're learning a language using Han characters, and you haven't known Han characters already, start with just the phonetic alphabets first. If you know them already... maybe starting with just phonetic alphabets would also be a good idea, but the difference won't be that significent.
 
