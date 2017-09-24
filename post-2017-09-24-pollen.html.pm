d3db2535416feaccf008b26ac918963b224f0c06
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-09-24 18:39:37 +0800

    New post

---
+++ post-2017-09-24-pollen.html.pm
@@ -0,0 +1,19 @@
+#lang pollen
+◊headline{Moving the blog, again}
+◊define-meta[publish-date]{2017/09/24 18:20}
+◊define-meta[categories]{English, Meta, Pollen, Frog}
+◊define-meta[language]{en}
+
+So, I restructured this blog again, this time using ◊link["https://mstill.io"] as a template, because I don't know how to use bootstrap. And I want to use Pollen markup.
+
+The process is actually suprisingly smooth, except:
+◊ul{
+◊li{Post date isn't updating. I have to fix it.}
+◊li{There's no Atom / RSS feed ready to use, I'll have to maybe do it myself.}
+}
+
+Overall I like this setup a bit more.
+
+◊i{Update: The not updating date was just me hard-coding the date during testing, then forgetting it there.}
+
+◊figure["images/newsite-directory.jpg" #:width "45rem"]{New site directory structure}
