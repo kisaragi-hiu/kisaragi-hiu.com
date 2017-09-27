e551447fd88f4771950760f90967c2966bd5cd31
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-09-27 12:01:21 +0800

    More info dump

--- UTAU Tutorial.html.pm
+++ UTAU Tutorial.html.pm
@@ -7,8 +7,9 @@
 ◊;define-meta[categories]{UTAU}
 ◊define-meta[toc]{true}
 
-Welcome to this UTAU tutorial. This is part 0, the Setup.
+Welcome to my UTAU tutorial.
 
+◊; Assumption
 I'm going to assume you know Kana and some Japanese already, 'cause I'm not going to explain Japanese in this series. If you don't, most of this should still make sense; I'd say utau is actually a good way to learn Kana. Anyways.
 
 ◊section["UTAU?"]
@@ -85,6 +86,10 @@ There are two modes for this, Mode1 is a curve relative to its key in the basic 
 To manipulate a note: S-drag = resize note no matter what, C-drag = resize note relative to adjacent note. Notes' position in time depends on lengths of all previous notes. Overlap has to be done with envelopes, and spacing has to be done with the rest note.
 
 Note properties can be changed with the menu, or use C-e for general properties, C-t for pitch curve settings, and C-y for envelope. C-r inserts a rest note, C-w selects all notes between the first and last non-rest notes, C-a selects all notes. C-x, C-c, C-v work as expected; C-v pastes after the current selected note. C-z and C-S-z are undo and redo, respectively. b is built-in editing tools, eg. Suffix Broker; n is plugin list. Just try every button on the keyboard with/without Ctrl held if you can't remember. Everything is in the toolbar as well.
+
+Turn on the option, then you can insert lyrics if there's only one note selected with enter. ◊(→→ "Enter" "type" "Enter")
+
+Space to play.
 
 Project settings: click the region below the utauloid's avatar, where their name is. This window will pop up.
 ◊figure["/images/utau/screenshot-project-settings.png" #:width "45rem"]{Project settings}
