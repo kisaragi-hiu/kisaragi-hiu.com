178d9d2d7127167a28a58711d98ce72ecd81e7af
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-10-08 22:40:57 +0800

    New post

---
+++ post/post-2017-10-08-densha.html.pm
@@ -0,0 +1,28 @@
+#lang pollen
+◊define-meta[headline]{飛逝}
+◊define-meta[publish-date]{2017/10/08 22:13}
+◊define-meta[categories]{詩}
+◊define-meta[comments]{true}
+
+◊;以電車景敘述人生，什麼的。
+
+窗外景色飛過
+我無法理解
+為何我會在此
+
+坐在窗邊
+回想你的表情
+幻想在你身邊
+感受不會消失的心傷
+
+思考邏輯
+記錄想法
+拍張照
+
+回到座位
+盯著眼前的現實
+理解
+
+我只是出現在這
+感受世界
+然後消失而已
