1ffb9ff553cdf5b51eff6f9703992ffb50084f4a
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-10-13 20:13:04 +0800

    More work on .travis.yml, deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+bash ./build.sh publish
+
+cd public || exit 1
+git init
+
+git config user.name "Built on Travis CI"
+git config user.email "flyingfeather1501@gmail.com"
+
+git add .
+git commit -m "Deploy to Github Pages"
+
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master >/dev/null 2>/dev/null
