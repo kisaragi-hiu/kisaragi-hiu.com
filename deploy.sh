fdeaf5a8b0cdff62e7433e272a7a6d5c99c1a9e0
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-10-14 21:10:19 +0800

    Try fixing site update

--- deploy.sh
+++ deploy.sh
@@ -11,4 +11,4 @@ git config user.email "flyingfeather1501@gmail.com"
 git add .
 git commit -m "Deploy to Github Pages"
 
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master >/dev/null 2>/dev/null
+git push "${GH_TOKEN}@${GH_REF}" master --force --quiet >/dev/null 2>/dev/null
