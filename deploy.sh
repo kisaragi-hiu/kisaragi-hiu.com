1c998b60ece22ccfb521326b3d5c786d3444fd45
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-10-15 17:11:34 +0800

    Don't use quiet flag when pushing.

--- deploy.sh
+++ deploy.sh
@@ -11,4 +11,4 @@ git config user.email "flyingfeather1501@gmail.com"
 git add .
 git commit -m "Deploy to Github Pages"
 
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master >/dev/null 2>&1
+git push --force "https://${GH_TOKEN}@${GH_REF}" master
