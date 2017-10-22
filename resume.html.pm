358bc3424000167243b5cd2948229056c4d0222a
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-10-23 00:38:34 +0800

    とりあえずrepoに保存して

---
+++ resume.html.pm
@@ -0,0 +1,35 @@
+#lang pollen
+◊h1{如月飛羽 (沈煜翔)}
+◊subtitle{
+flyingfeather1501@gmail.com}
+
+◊heading{education}
+◊(subtitle/sides "TZES" "2006~2012" "school")
+◊items{
+◊item{Taipei City Elementart School Sports Contest #3 in relay race, backup runner}
+}
+
+◊(subtitle/sides "DCSH" "2012~2015" "school")
+◊items{
+◊item{Linux interest}
+◊item{Started learning Japanese}
+}
+
+◊(subtitle/sides "SLHS" "2015~Present" "school")
+◊items{
+◊item{TOEIC 980}
+◊item{Start learning Lisp (Racket)}
+◊item{Translations}
+}
+
+◊heading{experience}
+◊items{
+◊item{osu!Sync zh-TW contributor}
+◊item{EasyRPG zh-TW maintainer}
+}
+
+◊heading{interests}
+◊items{
+◊item{Linux & FOSS}
+◊item{Language learning (Japanese, English)}
+}
