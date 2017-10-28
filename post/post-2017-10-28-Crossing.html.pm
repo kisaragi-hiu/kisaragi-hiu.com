05f2635cff1a3ff3485bca3a749a984bc656c710
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-10-28 18:59:48 +0800

    New post

---
+++ post/post-2017-10-28-Crossing.html.pm
@@ -0,0 +1,27 @@
+#lang pollen
+◊define-meta[headline]{Crossing}
+◊define-meta[language]{en}
+◊define-meta[publish-date]{2017/10/28 18:54}
+◊define-meta[comments]{true}
+◊define-meta[categories]{Poem}
+
+The green signal lights up
+As the overpass overlooks us
+Everyone has moved on
+As the signal turns red
+
+Let's go through the underpass
+
+Maybe someone's on the overpass
+Maybe you're on the overpass
+I can't know anyways
+I'm on a different route after all
+
+Everything goes its own route
+The eagle and the peng
+I've drifted off, while you're on the ground
+Without destination, the feather I am
+
+Coming out from the underpass
+Looking back at the crossing
+There's only a crowd, without you
