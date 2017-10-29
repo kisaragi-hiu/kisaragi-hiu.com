2f1f25092733c62438aa98a4960834291a130681
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-10-29 17:10:36 +0800

    Start fitting bio into this pollen.rkt

--- autobiography-en.html.pm
+++ autobiography-en.html.pm
@@ -1,21 +1,17 @@
 #lang pollen
-◊(require racket/string racket/format)
-◊(define slhs (link "http://www.slhs.tp.edu.tw/" "Shilin High School of Commerce"))
-◊(define dcsh (link "https://web.dcsh.tp.edu.tw/" "Da Zhi High School"))
-◊(define eos (link "https://elementary.io/" "elementary OS"))
+◊define-meta[headline]{About Kisaragi Hiu}
 ◊(define osu (link "https://osu.ppy.sh/home" "osu!"))
 ◊(define hoshinonamida (noichigo "1393204" "星の涙"))
-◊h1{About me: Kisaragi Hiu}
 
-◊topic{Past.}
+◊section{Past.}
 
-I'm Kevin Shen, or Kisaragi Hiu, a 17-year-old third-grader in ◊|slhs|.
+I'm Kevin Shen, or Kisaragi Hiu, a 17-year-old third-grader in ◊link["http://www.slhs.tp.edu.tw/"]{Shilin High School of Commerce}.
 
 I was born on February 28, 2000. According to my parents, I actually started memorizing the English alphabet before learning Bopomofo — just a trivia, not really an indicator for any sort of English skills.
 
 My parents sent me to a dual-language kindergarten when I was 3, where I received most of my early English education. Later the kindergarten course extended into elementary school, further driving most of my English learning of the period.
 
-◊subtopic{Elementary school}
+◊subsection{Elementary school}
 
 I attended ◊link["http://www.tzes.tp.edu.tw/"]{Taipei Municipal Da Zhi Elementary School} for my primary education. During this time, I spent most of my time getting good grades, and occasionally play a few video games. I was also very active, playing tag with friends every break between classes.
 
@@ -24,13 +20,13 @@ During grade 6, my class participated in the Taipei City Elementary School Sport
 I graduated in June 2012.
 ◊pagebreak
 
-◊subtopic{Junier high}
-I joined ◊|dcsh| in September.
+◊subsection{Junier high}
+I joined ◊link["https://web.dcsh.tp.edu.tw/"]{"Da Zhi High School"} in September.
 During grade 7, I started having an interest in installing custom ROMs on my Android phone, which grew into an interest in Linux a few months later. I started my Linux journey with Ubuntu, installing it on an unused computer at home, and learned a lot more about computers when using it. Around March 2013, I also started reading books on the writer ◊link["http://seba.tw"]{Seba's blog}, and I still read quite a lot to this day. I also played Minecraft for a bit, at one point trying (and failing) to create a custom game map in it.
 
 Fun trivia, my main email ◊code{kevinshencm10@gmail.com} was created during this period when I successfully installed CyanogenMod 10 on my Xperia Arc S for the first time.
 
-I got a laptop from my family in October 2013, and immediately installed ◊eos on it. After that was my time of rapid Linux exploration.
+I got a laptop from my family in October 2013, and immediately installed ◊link["https://elementary.io/"]{elementary OS} on it. After that was my time of rapid Linux exploration.
 
 I got into ◊osu in February 2014, and have been playing since.
 
@@ -38,26 +34,14 @@ I started a niconico account around April 2013, and got hooked onto the indie mu
 
 In grade 9, I started learning a vocal synthesizer platform called ◊link["http://utau2008.web.fc2.com/"]{UTAU}, and covered a few songs with it. I also started learning ◊link["https://www.blender.org/"]{Blender} around April 2014, and have finished a few renders with it.
 
-I publish my covers on ◊youtube["channel/UCl_hsqcvdX0XdgBimRQ6R3A" #:use-ref? #f]{Youtube} and ◊niconico["user/38995186" #:use-ref? #f]{niconico}, and my renders, sketches, etc. on ◊pixiv["member.php?id=14235181" #:use-ref? #f]{Pixiv}.
+I publish my covers on ◊youtube["channel/UCl_hsqcvdX0XdgBimRQ6R3A"]{Youtube} and ◊niconico["user/38995186"]{niconico}, and my renders, sketches, etc. on ◊pixiv["member.php?id=14235181"]{Pixiv}.
 
 ◊pagebreak
-◊subtopic{Senior high}
-◊;notes{
-◊;Joined ◊|slhs|, in the Applied Foreign Language (English) department, to improve my English skills.
-◊;
-◊;Grade 1, covered a few more songs with UTAU, and started playing around with ◊;◊link["https://blender.org"]{Blender}.
-◊;◊pixiv["flyingfeather1501"]{Here's my pixiv page} where I publish my pictures to.
-◊;
-◊;Grade 2, started to put more effort into programming.
-◊;July 2016: ◊github["flyingfeather1501/randomwallpaper"]{randomwallpaper}: wallpaper slideshow script
-◊;Also started practicing singing with my own voice in February 2017.
-◊;I publish my covers (and will hopefully publish more content in the future) on ◊;◊link["https://www.youtube.com/channel/UCl_hsqcvdX0XdgBimRQ6R3A"]{my Youtube channel}.
-◊;June 2017, I registered for the TOEIC test, and got a score of 980.
-◊;}
+◊subsection{Senior high}
 
-I joined ◊|slhs| in 2015, majoring in the Applied Foreign Language (English) department, with the goal of advancing my English skills.
+I joined ◊link["http://www.slhs.tp.edu.tw/"]{Shilin High School of Commerce} in 2015, majoring in the Applied Foreign Language (English) department, with the goal of advancing my English skills.
 
-I've translated a few projects in senior high so far. I am the main (only, actually) Chinese (Taiwan) translator for ◊link["https://easyrpg.org/"]{EasyRPG Player}, as you can see on ◊transifex["FlyingFeather" #:use-ref? #f]{my Transifex profile}, and one of the three Traditional Chinese translators for ◊github["naseweis520/osu-Sync"]{osu!Sync}, a tool for managing ◊osuwiki["Beatmaps"]{osu! beatmaps}.
+I've translated a few projects in senior high so far. I am the main (only, actually) Chinese (Taiwan) translator for ◊link["https://easyrpg.org/"]{EasyRPG Player}, as you can see on ◊transifex["FlyingFeather"]{my Transifex profile}, and one of the three Traditional Chinese translators for ◊github["naseweis520/osu-Sync"]{osu!Sync}, a tool for managing ◊osuwiki["Beatmaps"]{osu! beatmaps}.
 
 In July 2016, I started looking into programming, and made a Bash script called ◊github["flyingfeather1501/randomwallpaper"]{randomwallpaper} for slideshow wallpaper on some desktops that don't support it. I have since then written a few small shell utilities to automate my life a little bit better, like ◊code{markdown-furigana} which is a quick hack to replace all occurrences of ◊code{{{text:ruby}}} with html tags for ◊ruby{ruby◊rt{this}}.
 
@@ -66,14 +50,14 @@ All this time I've been playing ◊osu once in a while, but in March 2017 I deci
 After thinking about it since when I was 11 or so, I started practicing singing in February 2017. I have now practiced and even uploaded a few covers onto my youtube channel (though they're very rough around the edges). Currently I plan to join the school singing competetion in September and see how it goes.
 
 ◊pagebreak
-◊topic{Present.}
+◊section{Present.}
 Right now, I have the ability to:
 ･Translate English ↔ Mandarin, Japanese → English
-･Do basic programming (◊github["flyingfeather1501" #:use-ref? #f]{See for yourself.})
+･Do basic programming (◊github["flyingfeather1501"]{See for yourself.})
 ･Blender
 
 ◊pagebreak
-◊topic{Future.}
+◊section{Future.}
 ◊;notes{What do I want?
 ◊;I want to perfect my Japanese.
 ◊;I want to be a developer. Maybe Racket, maybe something to do with osu!, I dunno.
@@ -91,13 +75,8 @@ After that, it'll be about mixing my skills to create my own future. That's also
 
 ◊;notes{(Or at least that's what I intended. 5 years after its creation, I can definitely see where it's a bit silly. What does "fly like a feather" even mean?)}
 
-◊topic{Credits.}
+◊section{Credits.}
 This document is created with ◊link["http://pollenpub.com"]{Pollen}, a publishing system based on ◊link["http://racket-lang.org/"]{Racket}.
 Pollen is created by Matthew Butterick, the author of ◊link["http://practicaltypography.com/"]{Practical Typography}, ◊link["http://typographyforlawyers.com/"]{Typography for Lawyers}, and ◊link["http://beautifulracket.com/"]{Beautiful Racket}.
 
 The fonts used are ◊font["overpass"]{Overpass}, ◊font["opensans"]{Open Sans}, ◊font["firacode"]{Fira Code}, and ◊font["notosanscjk"]{Noto Sans CJK}.
-
-◊pagebreak
-◊topic{Reference.}
-◊;(string-join (hash-map references (lambda (x y) (string-append x ": " y))))
-◊(reference (map cdr (hash->list references)))
