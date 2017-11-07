aacd64200a76af6557e7978d05ed0be8f8f340a4
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-11-07 21:32:44 +0800

    Cleanup for frog

--- 404.html.pm
+++
@@ -1,8 +0,0 @@
-#lang pollen
-◊define-meta[template]{info-template.html}
-◊define-meta[headline]{Page not found.}
-
-This is the wrong place to be.
-
-◊link["/index.html"]{Go back to index.}
-
--- about.html.pm
+++
@@ -1,8 +0,0 @@
-#lang pollen
-◊define-meta[template]{info-template.html}
-◊define-meta[headline]{About me}
-
-有做一些翻譯，會華語，英文，聽得懂台語，讀得懂日文。大概。
-
-對Racket, Blender, Linux有興趣。  
-能夠用Bash弄出簡單的腳本，見我的◊link["https://github.com/flyingfeather1501"]{Github}.
