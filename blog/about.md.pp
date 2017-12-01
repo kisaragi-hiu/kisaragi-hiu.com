833129a1aa13734966c7f41916fd62e078b031b6
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-12-02 01:10:26 +0900

    Link to portfolio in about

--- blog/about.md.pp
+++ blog/about.md.pp
@@ -1,7 +1,11 @@
 #lang pollen
 # 關於我
 
+◊link["/portfolio.html" #:target "_self"]{My portfolio.}
+
 有做一些翻譯，會華語，英文，聽得懂台語，讀得懂日文。大概。
 
-對Racket, Blender, Linux有興趣。  
+◊newline-decode{
+對Racket, Blender, Linux有興趣。
 能夠用Bash弄出簡單的腳本，見我的◊link["https://github.com/flyingfeather1501"]{Github}.
+}
