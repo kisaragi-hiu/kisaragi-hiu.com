19fbc2fda0a010dfc7b5b7a43fcaf07d9b8c46bf
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-12-06 01:18:13 +0900

    New post: Hanezeve Caradhina language Day 1

---
+++ blog/posts/2017-12-05-Hanezeve-Caradhina-Day1.md.pp
@@ -0,0 +1,154 @@
+#lang pollen
+    Title: Making up a language from Hanezeve Caradhina, Day 1
+    Date: 2017-12-05T23:32:00
+    Tags: English, Language, Anime, Made in Abyss
+
+After listening to Hanezeve Caradhina for a few days, I decided it would be interesting to try to create a language based on the lyrics. 
+
+Listen to Hanezeve Caradhina (and the rest of the Made in Abyss OST) on Spotify: [https://open.spotify.com/album/5oEzjkSYSfC0ITRiqK6m8l](https://open.spotify.com/album/5oEzjkSYSfC0ITRiqK6m8l)
+
+The song doesn't actually have too much lyrics, as I will show in the next few paragraphs, which means this language isn't really going to be "deciphered". I'm simply going to create a new (very incomplete) language that barely makes sense. This is my progress in the first day.
+
+First, the lyrics.
+
+◊newline-decode{
+> A.
+> Halalaha zela halalelu
+> Sivivile shi-dhina
+> houwelela, lalelila
+> Hanezeve cara-dhina
+>
+> B.
+> Halalaha la forðe hala
+> helelele lelele
+> colwelela kalalila
+> Hanezeve yulumai ni
+>
+> A.
+> Halalaha zela halalelu
+> Sivivile shi-dhina
+> houwelela, lalelila
+> Hanezeve cara-dhina
+>
+> 'B.
+> Kodo wodo hondo hondo-oh
+> helelele lelele
+> colwelelo kalalila
+> Hanezeve yulumai ni
+>
+> A.
+> Halalaha zela halalelu
+> Sivivile shi-dhina
+> houwelela, lalelila
+> Hanezeve cara-dhina
+>
+}
+
+As you can see, it's all just two paragraphs with slight variations in B. I'll explain why I grouped syllables this way.
+
+There are a few patterns in the lyrics:
+
+- "Hanezeve" seems to be the focus.
+- "-dhina" appears quite a lot.
+- "colwele-la" progresses into "colwele-lo".
+- the syllables tend to be sung in groups of four.
+
+Unfortunately, there doesn't seem to be much more detail than this. The rest has to be imagined.
+
+## Deep in the Abyss
+
+Let's imagine that "Hanezeve cara-dhina" means "Deep in the Abyss". Then I can make up a grammar:
+
+- Hanezeve: the Abyss
+- -dhina: post-fix adjective for "deep"
+- cara-: particle meaning "in" or "down"
+
+That's a small part of the language thought up.
+
+Next, using the same sort of grammar, "yulu-maini" can be a preposition-adjective/adverb pair as well. Then imagine "Hanezeve yulumaini" means something about the bottom of the Abyss... seems like I can say that "yulu-" means "at" and "-maini" means "bottom".
+
+However, I'm a little stuck here. What next? What should this song be about? Until this point, I was thinking that this is a hymn dedicated to the Abyss, but maybe it's about Riko's journey?
+
+I now have to decide between "Riko's Journey" and "Hymn of the Abyss". Initially I was still thinking that Riko's Journey wouldn't suit the epic style this song has, but after listening to it a few more times and imagining the progression, it started to feel like a journey more and more. I eventually decided the vocal and the instruments combined suits "Riko's Journey" better.
+
+With that, "Hanezeve yulumaini" probably shouldn't just be a noun phrase. Plus, that "ni" really gives me the urge to connect it with the Japanese particle "に", ie. "to". Let's imagine it means "To the bottom of the Abyss". Now:
+
+- Hanezeve: the Abyss
+- yulu-: particle meaning "at" or "of", sort of
+- -mai: post-fix adjective for "bottom"
+- -ni: analogous to Japanese "に", meaning "to"
+
+Using this grammar, I can read `Hanezeve cara-dhina` as `Abyss in-deep`, and `Hanezeve yulu-mai ni` as `Abyss at-bottom to`. Progress!
+
+## The wonder
+
+Another sentence I've managed to make up a structure, then somewhat make sense of, is:
+
+◊newline-decode{
+> Sivivile shi-dhina
+> houwelela, lalelila
+}
+
+I started by just listening to the vocal, thinking about the main idea of the song ("Riko's Journey"), and imagining what English lyrics would be fitting. After a bit, I came up with:
+
+◊newline-decode{
+> Explore even deeper
+> ...
+}
+
+which fits both Riko's desire to go deeper, and the grammar structure I established with -dhina in the previous section. So:
+
+- Sivivile: verb, "explore"
+- shi-: adverb/adjective modifier, "even" as in "even more"
+- -dhina: post-fix adverb/adjective, "deep"
+
+As of the other two words, I just thought up "treasure" and "wonder" as two nouns suitable for "explore the Abyss even deeper". Lalelila sounds more like wonder in my mind for how it repeats `l-vowel` four times with the vowel going around (a-e-i-a), so I made it "wonder", and houwelela "treasure".
+
+- houwelela: noun, treasure
+- lalelila: noun, wonder
+
+## What I have now
+
+◊newline-decode{
+> A.
+> Halalaha zela halalelu
+> Explore even deeper
+> the treasure, the wonder
+> Deep in the Abyss
+>
+> B.
+> Halalaha la forðe hala
+> helelele lelele
+> colwelela kalalila
+> To the Bottom of the Abyss
+>
+> A.
+> Halalaha zela halalelu
+> Explore even deeper
+> the treasure, the wonder
+> Deep in the Abyss
+>
+> 'B.
+> Kodo wodo hondo hondo-oh
+> helelele lelele
+> colwelelo kalalila
+> To the Bottom of the Abyss
+>
+> A.
+> Halalaha zela halalelu
+> Explore even deeper
+> the treasure, the wonder
+> Deep in the Abyss
+>
+}
+
+## More to come (hopefully)
+
+There are still many regularities with the rest of the lyrics.
+
+- Halalaha zela halalelu?
+- 'B changed the first line quite a bit, what should that mean?
+- colwelela → colwelelo?
+- kalalila?
+
+And I'm sure trying to make up something would be a nice pass-time for me for the next few weeks or something. So hopefully I can keep this up and eventually finish this small language.
