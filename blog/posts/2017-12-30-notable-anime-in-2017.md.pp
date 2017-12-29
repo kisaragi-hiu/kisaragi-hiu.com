700bfc7b98b563be9e5ed30b8e0bd0c6ea457d6f
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-12-30 01:26:06 +0900

    Add new draft '2017 notable anime'

---
+++ blog/posts/2017-12-30-notable-anime-in-2017.md.pp
@@ -0,0 +1,36 @@
+#lang pollen
+    Title: My list of notable anime series in 2017
+    Date: 2017-12-30T01:06:27
+    Tags: language:en, DRAFT, Anime
+
+2017 is the second year since I started following seasonal anime. (I started with Re:0 last summer.)
+
+There have been a few series that I liked this year, and this is the list.
+
+## Chaos;Child
+
+This series was… weird. Being a sequal to Chaos;Head, I guess that's to be expected. This was kind of a mediocre series to me.
+
+## Youjo Senki
+
+When this series aired, I originally thought this is just another Loli shit that has no idea what it's talking about, judging by its title. I was wrong.
+
+I decided to pick up this series after being spoiled a bit about Tonya's backstory in a review on Youtube. It really knows what it's doing.
+
+Tonya is absolutely amazing.
+
+I wish I could see its actual ending though. That's the problem with adaptations…
+
+## Sakurada Reset
+
+I love this series. Fight me.
+
+Maybe it's because how I *somehow* relate to Haruki, maybe it's the English version that sucked… I think this is a good story (and at least has actually ended).
+
+It's adapted into a movie (series) and an anime at the same time, but I've heard that the movie was heavily abridged… They really should have put the budget into the anime instead of the movie.
+
+I do agree the animation leaves a lot to be desired. The background characters almost never move — this is quite a problem when there is a scene where those people were frozen by some superpower.
+
+## Sukasuka
+
+The *stomach aching* series of Spring 2017.
