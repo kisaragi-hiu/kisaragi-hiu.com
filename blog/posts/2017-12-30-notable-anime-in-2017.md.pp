db54b8cbad1d278c96e1864bf530ec85442ccbe2
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-12-31 02:21:24 +0900

    2017 notable anime: temporary ◊series tag; some text edits

--- blog/posts/2017-12-30-notable-anime-in-2017.md.pp
+++ blog/posts/2017-12-30-notable-anime-in-2017.md.pp
@@ -3,34 +3,51 @@
     Date: 2017-12-30T01:06:27
     Tags: language:en, DRAFT, Anime
 
+◊(require txexpr)
+◊(define (series text)
+   (->html (txexpr* 'div '()
+                    `(span ([style "font-weight: 700;"]) ,text)
+                    `(hr ([style
+                            ,(string-append "height: 1px;"
+                                            "width: 6rem;"
+                                            "margin: 0.3rem auto 1.2rem;"
+                                            "text-align: left;"
+                                            "margin-right: 100%;"
+                                            "background-color: #444;")])))))
+
 2017 is the second year since I started following seasonal anime. (I started with Re:0 last summer.)
 
 There have been a few series that I liked this year, and this is the list.
 
-## Chaos;Child
+◊series{Saga of Tanya the Evil (Youjo Senki)} This is actually a story about an alternative version of World War I in a world with semi-magical powers.
 
-This series was… weird. Being a sequal to Chaos;Head, I guess that's to be expected. This was kind of a mediocre series to me.
-
-## Youjo Senki
-
-When this series aired, I originally thought this is just another Loli shit that has no idea what it's talking about, judging by its title. I was wrong.
-
-I decided to pick up this series after being spoiled a bit about Tonya's backstory in a review on Youtube. It really knows what it's doing.
-
-Tonya is absolutely amazing.
+When this series aired, I thought this is just another loli series that kind of just screws around. Then a few months after the broadcast, I decided to pick up this series after watching a review of it (and being spoiled) on Youtube. It was actually quite nice.
 
 I wish I could see its actual ending though. That's the problem with adaptations…
 
-## Sakurada Reset
+◊series{Sakurada Reset} I love this series. Maybe it's because how I *somehow* relate to Haruki, maybe it's the English version that sucked… Fight me.
 
-I love this series. Fight me.
-
-Maybe it's because how I *somehow* relate to Haruki, maybe it's the English version that sucked… I think this is a good story (and at least has actually ended).
+This is a story taking place in a superpower-filled town, about solving the mysteries two of the main cast face.
 
 It's adapted into a movie (series) and an anime at the same time, but I've heard that the movie was heavily abridged… They really should have put the budget into the anime instead of the movie.
 
-I do agree the animation leaves a lot to be desired. The background characters almost never move — this is quite a problem when there is a scene where those people were frozen by some superpower.
+I do agree the animation leaves a lot to be desired though. There are way too many still frames, even when they could have at least added a little bit of wind or camera movements.
 
-## Sukasuka
+◊series{Sukasuka} The *stomach aching* series of Spring 2017.
 
-The *stomach aching* series of Spring 2017.
+◊series{Made in Abyss}
+
+◊series{Fukumenkei Noise}
+
+◊series{Konosuba S2}
+
+◊series{Kino's Journey}
+
+◊series{Shoujo Shuumatsu Ryokou} Interesting to see the 
+
+◊series{Houseki no Kuni} Huh, a full 3dcg series! Let's see… → WTF this is *really* good.
+
+A genderless society (of about 30 people)! Yay!
+
+◊series{Just Because} The relationships actually don't feel too lame or forced into a story.
+
