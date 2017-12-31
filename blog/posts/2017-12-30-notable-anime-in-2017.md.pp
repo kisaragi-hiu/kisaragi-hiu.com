d8f0bf29a17c1a580d39beb1e7fc70cdbba9159a
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-12-31 23:07:52 +0900

    Update 2017 notable anime

--- blog/posts/2017-12-30-notable-anime-in-2017.md.pp
+++ blog/posts/2017-12-30-notable-anime-in-2017.md.pp
@@ -1,6 +1,6 @@
 #lang pollen
     Title: My list of notable anime series in 2017
-    Date: 2017-12-30T01:06:27
+    Date: 2017-12-31T01:06:27
     Tags: language:en, DRAFT, Anime
 
 ◊(require txexpr)
@@ -15,9 +15,13 @@
                                             "margin-right: 100%;"
                                             "background-color: #444;")])))))
 
+◊(define (rant . text)
+   (->html `(span ([style "color: #777;"]) "(" ,@text ")")))
+
 2017 is the second year since I started following seasonal anime. (I started with Re:0 last summer.)
 
 There have been a few series that I liked this year, and this is the list.
+
 
 ◊series{Saga of Tanya the Evil (Youjo Senki)} This is actually a story about an alternative version of World War I in a world with semi-magical powers.
 
@@ -33,17 +37,39 @@ It's adapted into a movie (series) and an anime at the same time, but I've heard
 
 I do agree the animation leaves a lot to be desired though. There are way too many still frames, even when they could have at least added a little bit of wind or camera movements.
 
-◊series{Sukasuka} The *stomach aching* series of Spring 2017.
+Oh, and Souma Sumire's voice is *so* great.
 
-◊series{Made in Abyss}
+◊series{Sukasuka} Series like this are why I love fantasy stories. I just love daydreaming in those wonderfully constructed worlds. The world Sukasuka takes place in is one that's actually quite hopeless, even compared to our own. This is the *stomach aching* series of Spring 2017.
 
-◊series{Fukumenkei Noise}
+I really like how the animation is focused on Chtholly ◊rant{can never remember how her name's spelled…}, while the light novel focuses on Willem.
 
-◊series{Konosuba S2}
+◊series{Made in Abyss} Colwelela (Hanezeve yulu-mai) kalalila!
 
-◊series{Kino's Journey}
+◊rant{I'm two lines away from finishing making up the language of Hanezeve Caradhina, really should finish that soon…}
 
-◊series{Shoujo Shuumatsu Ryokou} Interesting to see the 
+I used to dream about an abandoned house in a hazy forest, thinking about how nice it'd be to explore somewhere like that. This story gives me the same feeling.
+
+With the second season announced (pretty sure it'll still be seasonal anime), I'm looking forward to following this series in 2018.
+
+◊series{Fukumenkei Noise} This series aired while I was trying out practicing singing (which I currently put on hold), so it sort of grabbed my attention being a story around three people involved in music. ◊rant{I just noticed Yuzu goes by the same name as the protagonist in Citrus, how ◊s{wonderful}…}
+
+Being a manga adaptation (that isn't extremely successful), the manga is where all the future updates will go. I do like how this story was set up, but hope that I could see more…
+
+◊series{Konosuba S2} Being one of the more well made comedy series of recent time (?), Konosuba doesn't really *need* a second season to be an enjoyable ride, but this season lives up to its promise. It really is quite fun to watch.
+
+That said, I personally still prefer deeper stories, with characters to relate to and relationships / motivations to figure out.
+
+◊series{Kino's Journey (Kino no tabi)} Kino's Journey has always been its own weird beast that somehow works. The novel series has been going on for almost as long as I've lived…!
+
+To be honest, the way the countries treat a traveller seems a bit unnatural to me… but that can easily be explained by how the countries are kind of just spread out everywhere. There might not be as many people travelling around like on earth.
+
+I wouldn't go so far as to say I *love* this series, but it's certainly really confortable to watch (and read).
+
+◊series{Girl's Last Tour (Shoujo Shuumatsu Ryokou)} Kino's Journey, Post-apocalyptic edition (?).
+
+These two stories *kind of* work the same way, although Shuumatsu is focused much more on the interactions between the two protagonists. They both just kind of wander around into adventures, and don't really have a real goal (at least initially).
+
+I particularly really like how the city Chito and Yuuri travels in is portrayed. By the way, an episode of ◊youtube["channel/UCZFipeZtQM5CKUjx6grh54g"]{“Science and Futurism with Isaac Arthur”} actually talked about a concept *probably physically possible* similar to this story's, that involves building ◊youtube["watch?v=ioKidcpkZN0"]{“Mega Earths”} with layers and layers of living space.
 
 ◊series{Houseki no Kuni} Huh, a full 3dcg series! Let's see… → WTF this is *really* good.
 
