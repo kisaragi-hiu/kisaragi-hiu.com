1e07619c577afee8ed3c50283ad308eb7fc73558
Kisaragi Hiu <flyingfeather1501@gmail.com>
2018-01-15 20:55:25 +0900

    Move video in sg-input-workaround after the second paragraph

--- blog/posts/2018-01-15-sg-input-workaround.md.pp
+++ blog/posts/2018-01-15-sg-input-workaround.md.pp
@@ -3,11 +3,11 @@
     Date: 2018-01-15T13:51:00
     Tags: language:en, category:kisaragi, Linux, Games, Wine
 
-◊video/gif-esque["/videos/sg-spammed-key-showcase.mp4"]
-
 Stein's Gate was released on Steam on September 9, 2016, for Windows only. A few months later, as far as I can remember, Wine started being able to run it.
 
 However, for the entirety of 2017, I haven't been able to play it: mouse selection doesn't work at all, menu entries are selected then deselected immediately without my input, the menu is completely unusable. It's almost as if some input is being spammed…?
+
+◊video/gif-esque["/videos/sg-spammed-key-showcase.mp4"]
 
 I found the culprit just last week. There's something wrong with Wacom tablet handling in this game, and unplugging the tablet before starting it completely removed the problems. I can't believe I haven't tried this for the longest time…!
 
