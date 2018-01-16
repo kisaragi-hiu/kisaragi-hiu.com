6fc5fe9e5cfbe6ad637200f1a7f32925fe9e4082
Kisaragi Hiu <flyingfeather1501@gmail.com>
2018-01-16 21:54:02 +0900

    'Stein's' -> 'Steins;'

--- blog/posts/2018-01-15-sg-input-workaround.md.pp
+++ blog/posts/2018-01-15-sg-input-workaround.md.pp
@@ -1,9 +1,9 @@
 #lang pollen
-    Title: Working around Stein's Gate's input problem on Linux
+    Title: Working around Steins;Gate's input problem on Linux
     Date: 2018-01-15T13:51:00
     Tags: language:en, category:kisaragi, Linux, Games, Wine
 
-Stein's Gate was released on Steam on September 9, 2016, for Windows only. A few months later, as far as I can remember, Wine started being able to run it.
+Steins;Gate was released on Steam on September 9, 2016, for Windows only. A few months later, as far as I can remember, Wine started being able to run it.
 
 However, for the entirety of 2017, I haven't been able to play it: mouse selection doesn't work at all, menu entries are selected then deselected immediately without my input, the menu is completely unusable. It's almost as if some input is being spammed…?
 
