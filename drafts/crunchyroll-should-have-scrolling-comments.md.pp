ca9d83425efa2bc9351c713a7527d7d38101f893
Kisaragi Hiu <mail@kisaragi-hiu.com>
2018-03-15 01:39:58 +0900

    Add draft about scrolling comments

---
+++ drafts/crunchyroll-should-have-scrolling-comments.md.pp
@@ -0,0 +1,8 @@
+#lang pollen
+    Title: ◊title◊
+    Date: ◊date◊
+    Tags: category:◊category◊, language:en, ◊tags◊
+
+Crunchyroll (and other video services) should implement danmaku (scrolling comments)
+
+◊; vim: filetype=pollen
