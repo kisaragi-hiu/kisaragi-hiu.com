dbac3922667f4b192f9434e6205203c2879bbf50
Kisaragi Hiu <mail@kisaragi-hiu.com>
2018-04-08 18:54:10 +0900

    update drafts

--- drafts/crunchyroll-should-have-scrolling-comments.md.pp
+++ drafts/crunchyroll-should-have-scrolling-comments.md.pp
@@ -5,4 +5,9 @@
 
 Crunchyroll (and other video services) should implement danmaku (scrolling comments)
 
+◊section{What is danmaku?}
+◊section{What are the benefits of implementing a danmaku system?}
+◊section{How are various sites implementing danmaku?}
+◊section{}
+
 ◊; vim: filetype=pollen
