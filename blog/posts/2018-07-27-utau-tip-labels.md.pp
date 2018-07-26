35c48dfd1a58a7c39d53c725afdde25c55a6c068
Kisaragi Hiu <mail@kisaragi-hiu.com>
2018-07-27 00:23:04 +0900

    Add UTAU labels post

---
+++ blog/posts/2018-07-27-utau-tip-labels.md.pp
@@ -0,0 +1,30 @@
+#lang pollen
+    Title: UTAU tip: labels
+    Date: 2018-07-27T00:23:19
+    Tags: language:en, category:Tutorials, UTAU
+
+In UTAU, labels are a simple way to mark sections of the UST to easily jump around. I've been using them to make notes in the UST since [my メリュー cover](https://www.youtube.com/watch?v=PNLT7WUuXHM), and for marking sections since [my 濫觴生命 cover](https://www.youtube.com/watch?v=3hnI8DqMVfc).
+
+◊image["/images/utau/screenshot-labels.png" "Labels marking sections."]
+
+For some reason, I've never seen a UST with section marks, even though I think they're quite helpful.
+
+Label is a note property, this makes it useful for marking specific notes as well.
+
+## Using labels
+
+Right click on the top line where the bar numbers are, then select `Set Label Here`.
+
+◊image["/images/utau/screenshot-labels-set.png"]
+
+To remove or change a label, right click on it again then select the appropriate option.
+
+To jump to a label, right click on the top line, select `Move`, then choose the label to jump to.
+
+◊image["/images/utau/screenshot-labels-change.png" "Move, Change label, and Remove label."]
+
+## Example
+
+◊image["/images/utau/screenshot-labels-usage.png" "My labels usage in the 濫觴生命 cover."]
+
+In this UST I've named various sections with labels (Intro, Verse, etc.) so I can easily jump to them. I also have a few labels marking specific notes, for instance `here` is what I use to mark my progress through the file, and `???` probably marks some notes sounding weird.
