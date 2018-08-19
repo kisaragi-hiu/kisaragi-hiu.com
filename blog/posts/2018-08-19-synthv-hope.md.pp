09e054d5cec1884d612cf234b0f111b3577d5704
Kisaragi Hiu <mail@kisaragi-hiu.com>
2018-08-19 17:49:44 +0900

    Add new post

---
+++ blog/posts/2018-08-19-synthv-hope.md.pp
@@ -0,0 +1,12 @@
+#lang pollen
+    Title: SynthesizerV hopes before trying it out
+    Date: 2018-08-19T17:47:32
+    Tags: language:en, category:Opinion, SynthesizerV, UTAU
+
+Before trying out #SynthV Technical Preview, I'm hoping for custom VB support, maybe even for UTAU ones. 
+
+A big reason why CeVIO never got as big as UTAU is because it's needlessly hard to make VBs for it (just like VOCALOID). People want to hear their favorite characters sing, not just to hear nicely tuned songs, but CeVIO only has one popular character: IA's sister. Whereas VOCALOID attracted companies to make many VBs for it and got many popular characters, CeVIO, being a newcomer, hasn't.
+
+It's going to be hard for SynthV to get new and potentially popular characters if there is no custom VB support. (UTAU VB support could be a bonus, but I understand UTAU's VB format is not that simple.)
+
+(I posted this as [a Twitter thread](https://twitter.com/flyin1501/status/1031078524830932993))
