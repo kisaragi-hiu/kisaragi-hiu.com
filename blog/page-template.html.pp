fb189ef4d7c3573081e8497b79cbed04311c3d36
Kisaragi Hiu <mail@kisaragi-hiu.com>
2018-10-05 01:45:50 +0800

    Go back to Skeleton.css for now

--- blog/page-template.html.pp
+++ blog/page-template.html.pp
@@ -38,7 +38,9 @@
     @(when rel-prev @list{<link rel="prev" href="@|rel-prev|">})
     ◊; Font
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Fira+Sans|Overpass+Mono|Overpass:400,600">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="@|uri-prefix|/css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="@|uri-prefix|/css/skeleton.css">
     <link rel="stylesheet" type="text/css" href="@|uri-prefix|/css/emacs.css">
     <link rel="alternate" type="application/atom+xml"
           href="@|atom-feed-uri|" title="Atom Feed">
@@ -111,15 +113,6 @@
         ◊(->html ◊p{Style based on ◊a[#:href "http://getskeleton.com/"]{Skeleton}.})
       </footer>
     </div>
-    ◊script[#:src "https://code.jquery.com/jquery-3.3.1.slim.min.js"
-            #:integrity "sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-            #:crossorigin "anonymous"]
-    ◊script[#:src "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
-            #:integrity "sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-            #:crossorigin "anonymous"]
-    ◊script[#:src "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
-            #:integrity "sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
-            #:crossorigin "anonymous"]
     ◊; Google Analytics
     <script>
       window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
