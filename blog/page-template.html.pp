9b9a0bf8bc97b1c6a85d4744d9bc1a3af6d10f8c
Kisaragi Hiu <mail@kisaragi-hiu.com>
2018-10-05 09:53:56 +0800

    Fix @title

--- blog/page-template.html.pp
+++ blog/page-template.html.pp
@@ -26,7 +26,7 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>@title</title>
+    <title>@|title|</title>
     <meta name="description" content="@|description|">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="author" content="@|author|">
