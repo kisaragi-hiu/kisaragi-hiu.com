cc2ba8037a33fa305d6c35fe8b2083718f819efe
Kisaragi Hiu <mail@kisaragi-hiu.com>
2018-10-06 01:35:14 +0800

    Use Pollen on main.css and ignore main.css in css/

---
+++ blog/css/main.css.pp
@@ -0,0 +1,27 @@
+#lang pollen
+◊(require racket/string)
+◊(define cjk-fallback
+   ◊string-append{'Noto Sans CJK TC',
+                  'Microsoft Jhenghei',
+                  'Microsoft Yahei',
+                  'Meiryo',
+                  'Malgun Gothic'})
+◊(define sans-serif
+   ◊string-append{'Fira Sans',
+                  ◊|cjk-fallback|,
+                  'sans-serif'})
+◊(define title-sans-serif
+   ◊string-append{'Overpass',
+                  ◊|cjk-fallback|,
+                  'sans-serif'})
+◊(define monospace
+   ◊string-append{'Overpass Mono',
+                  'Noto Sans Mono CJK',
+                  ◊|cjk-fallback|,
+                  'monospace'})
+◊(define title "h1,h2,h3,h4,h5,h6,h7,.title")
+
+#logo img {
+    max-height: 2em;
+    margin-right: 0.4em;
+}
