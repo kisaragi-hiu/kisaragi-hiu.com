3b1df9d639efc1677b14211992f84a7b5de8c97e
Kisaragi Hiu <mail@kisaragi-hiu.com>
2018-11-04 00:43:47 +0800

    move 2018-08-19 post to 2018-11-04, write down that I had forgotten this post

--- blog/posts/2018-11-04-synthv-hope.md.pp
+++ blog/posts/2018-11-04-synthv-hope.md.pp
@@ -1,6 +1,6 @@
 #lang pollen
     Title: SynthesizerV hopes before trying it out
-    Date: 2018-08-19T17:47:32
+    Date: 2018-11-04T00:37:50
     Tags: language:en, category:Opinion, SynthesizerV, UTAU
 
 Before trying out #SynthV Technical Preview, I'm hoping for custom VB support, maybe even for UTAU ones. 
@@ -9,4 +9,4 @@ A big reason why CeVIO never got as big as UTAU is because it's needlessly hard 
 
 It's going to be hard for SynthV to get new and potentially popular characters if there is no custom VB support. (UTAU VB support could be a bonus, but I understand UTAU's VB format is not that simple.)
 
-(I posted this as [a Twitter thread](https://twitter.com/flyin1501/status/1031078524830932993))
+(I initially posted this as [a Twitter thread](https://twitter.com/flyin1501/status/1031078524830932993). I then let it sit in a branch, unpushed, for almost three months. I've since tried SynthV; will put up my thoughts later.)
