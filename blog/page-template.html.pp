ef341db77935a8d92ad99340d5aff100b0b6b897
Kisaragi Hiu <mail@kisaragi-hiu.com>
2018-11-11 00:44:15 +0800

    Add title back onto header

--- blog/page-template.html.pp
+++ blog/page-template.html.pp
@@ -66,12 +66,13 @@
       ◊; Header
       <header id="header" class="py-2">
         <div class="row flex-nowrap justify-content-between alien-items-center">
-          <div id="logo" class="col-1 pt-1">
+          <div id="logo" class="col-6 pt-1">
             <div class="d-flex justify-content-begin">
               <a href="/" target="_self" class="py-2 pr-2"><img src="/images/avatar.png" alt="Kisaragi Hiu"/></a>
+              <h1 style="margin-top: 0.4em;">Kisaragi&nbsp;Hiu</h1>
             </div>
           </div>
-          <div class="col-11 nav-scroller py-1">
+          <div class="col-6 nav-scroller py-1">
             <nav class="nav d-flex justify-content-end">
               <a class="p-2 text-secondary" href="/">Blog</a>
               @(apply
