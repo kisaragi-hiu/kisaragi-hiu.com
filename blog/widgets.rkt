34d46d1b6a8f770d2922a5b5dbf5234579d000f0
Kisaragi Hiu <mail@kisaragi-hiu.com>
2018-11-11 00:33:12 +0800

    implement translate-inline

--- blog/widgets.rkt
+++ blog/widgets.rkt
@@ -182,7 +182,26 @@
   (define str (string-join strings ""))
   (~> (translation-dict str) 'en))
 
+;; ◊$[#:en "Eng" #:zh "華"] ->
+;; '(span ([class "translate"])
+;;   (span ([class "lang-en"]) "Eng")
+;;   (span ([class "lang-zh"]) "華"))
+(define translate-inline
+  (make-keyword-procedure
+   (lambda (kws kw-args . rest)
+     (~> (map (lambda (kw kw-arg)
+                `(span
+                  ([class ,(~a "lang-" kw)])
+                  ,kw-arg))
+              kws kw-args)
+         (cons 'span _)
+         (attr-set _ 'class "translate")
+         ;; There's no statisfying way to pass this in with a keyword.
+         ;; Use parameterize instead.
+         ((if (current-return-txexpr?) identity ->html) _)))))
+
 (define ! translate)
+(define $ translate-inline)
 
 ;;; Link functions
 
