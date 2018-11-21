df3fb897106cbb4e16dba443789d1f35fc00661f
Kisaragi Hiu <mail@kisaragi-hiu.com>
2018-11-21 17:07:41 +0800

    port two more posts

---
+++ just-pollen/blog/2017-03-19-new-website.html.pm
@@ -0,0 +1,14 @@
+#lang pollen
+◊define-meta[title]{新網站}
+◊define-meta[date]{2017-03-19T05:00:00}
+◊define-meta[category]{Meta}
+◊define-meta[language]{zh-TW}
+
+想要有個簡單的檔案記錄所有連結◊(br)
+→放在Github Pages好了◊(br)
+→試試看Jekyll好了◊(br)
+→還是直接弄成完整的網站吧◊(br)
+什麼的。◊(br)
+
+結果就這樣的，Blogger那裡正式的被取代掉了呢。◊(br)
+總之，新網站。
---
+++ just-pollen/blog/2017-06-28-redire.html.pm
@@ -0,0 +1,13 @@
+#lang pollen
+◊define-meta[title]{レディーレ Cover}
+◊define-meta[date]{2017-06-28T22:50:00}
+◊define-meta[category]{Own Vocal Experiment}
+◊define-meta[language]{zh-TW}
+
+試唱了バルーン的レディーレ。◊(br)
+看到本家就想要唱唱看了，但拖了兩個禮拜才全部弄好就是ww
+
+這次回到用 Ardour 來混音了。◊(br)
+剛開始感覺比 Audacity 麻煩一點，沒辦法直接做正規化等等，但當效果都加好之後，有段要重錄的話錄好就完成了，不用所有 sample 重新來過。
+
+◊youtube/embed{PEWtJbIswWA}
