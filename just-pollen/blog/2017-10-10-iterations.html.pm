7d926bc734d4b7aeef57bce51d506a4e46f2ebe1
Kisaragi Hiu <mail@kisaragi-hiu.com>
2018-11-27 22:00:52 +0800

    port three more posts

---
+++ just-pollen/blog/2017-10-10-iterations.html.pm
@@ -0,0 +1,32 @@
+#lang pollen
+◊define-meta[title]{Iterations of Learning}
+◊define-meta[date]{2017-10-10T00:00:00}
+◊define-meta[category]{Opinions}
+◊define-meta[language]{en}
+
+Learning is based on iterations. Do something, then repeat, then repeat until you're familiar with it, is essentially the process; people call this "practice".
+
+Different subjects have different sorts of steps to repeat, for instance:
+
+
+◊h3{Language learning}
+
+1. Find a unit of text (eg. a sentence).
+
+2. Figure out what it means.
+
+3. Repeat.
+
+◊h3{Piano}
+
+1. Find an unfamiliar sheet.
+
+2. Divide it into smaller chunks.
+
+3. Play each chunk until some level of familiarity.
+
+4. Combine them together.
+
+5. Repeat.
+
+Learning is essentially finding the steps of an iteration, and then doing it again and again.
