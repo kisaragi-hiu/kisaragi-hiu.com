a62f44921c2806f3347a7255739472f592d95801
Kisaragi Hiu <mail@kisaragi-hiu.com>
2018-12-12 01:12:33 +0800

    fix type errors in sample category page

--- category/opinion.html.pm
+++ category/opinion.html.pm
@@ -5,6 +5,10 @@
 ◊define-meta[type]{category-index}
 
 ◊(~> (children 'blog (current-pagetree))
-     (filter (lambda (p) (string-ci=? (select-from-metas 'category p) "opinion")) _)
+     (filter
+       (lambda (p)
+        (and (string? (select-from-metas 'category p))
+             (string-ci=? (select-from-metas 'category p) "opinion")))
+      _)
      (map index-item _)
      (txexpr 'div '([class "index"]) _))
