e9692dc1842030fe62f25d7cdf9fa6622087e7aa
Kisaragi Hiu <mail@kisaragi-hiu.com>
2018-12-13 21:37:27 +0800

    add mostly empty about page

---
+++ about.html.pm
@@ -0,0 +1,6 @@
+#lang pollen
+◊define-meta[type]{page}
+◊define-meta[title]{About}
+◊heading{About}
+
+I'm a college student interested in Free Software, programming, VOCALOID / UTAU culture, and language learning.
