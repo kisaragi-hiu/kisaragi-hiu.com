a80cecced0a3e2922a3d11d820a5beb2dcb05e3b
Kisaragi Hiu <mail@kisaragi-hiu.com>
2018-12-24 00:14:27 +0900

    add some works in projects.html

--- projects.html.pm
+++ projects.html.pm
@@ -5,4 +5,18 @@
 
 ◊link{https://github.com/kisaragi-hiu}
 
-To be expanded.
+◊subheading{◊link["https://github.com/kisaragi-hiu/cangjie.el"]{Cangjie.el}}
+
+Retrieve Cangjie code for Han character in Emacs.
+
+◊subheading{◊link["https://github.com/kisaragi-hiu/tr.el"]{Tr.el}}
+
+Minimal tr implementation for Emacs.
+
+◊subheading{◊link["https://gitlab.com/kisaragi-hiu/scripts"]{Scripts}}
+
+Small scripts I've written over the years.
+
+◊subheading{◊link["https://gitlab.com/kisaragi-hiu/language-startup-benchmark"]{Language Startup Benchmark}}
+
+Time hello world in various languages to benchmark their startup times.
