d58be554020decbb083bc657412017e6d4c55621
Kisaragi Hiu <mail@kisaragi-hiu.com>
2019-02-16 01:36:42 +0900

    new draft

---
+++ danhai-lrt-screen.html.pm
@@ -0,0 +1,9 @@
+#lang pollen
+◊define-meta[title]{淡海輕軌標記}
+◊define-meta[date]{2019-02-16T01:23:09+09:00}
+◊define-meta[category]{Opinion}
+◊define-meta[language]{zh-tw}
+
+農曆新年時我去搭了一次淡海輕軌。除了進出站有不同的刷卡機和明明沒有要自動開門卻又要響到站/開門的警告聲以外，我看到最大的問題時車廂內的螢幕。
+
+◊image["/static/image/danhai-lrt-screen-original.jpg"]
