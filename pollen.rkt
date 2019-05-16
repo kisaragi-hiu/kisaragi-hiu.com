cad650589497de28a78f682e04cb520e694b7935
Kisaragi Hiu <mail@kisaragi-hiu.com>
2019-05-17 00:10:04 +0900

    even with render cache turned on it's still taking too long on travis

--- pollen.rkt
+++ pollen.rkt
@@ -31,9 +31,9 @@
           racket/list
           txexpr))
 
-(module+ setup
-  (provide (all-defined-out))
-  (define compile-cache-active #f))
+;; (module+ setup
+;;   (provide (all-defined-out))
+;;   (define compile-cache-active #f))
 
 (define author "Kisaragi Hiu")
 (define site-title "Kisaragi Hiu")
