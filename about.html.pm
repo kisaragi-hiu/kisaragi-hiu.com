1176cfd9ed2d81b1098f4b23aa6db63003553434
Kisaragi Hiu <mail@kisaragi-hiu.com>
2019-11-01 15:51:05 +0900

    update links in About

--- about.html.pm
+++ about.html.pm
@@ -2,18 +2,47 @@
 ◊define-meta[type]{page}
 ◊define-meta[title]{About}
 
-I'm a college student interested in Free Software, programming, VOCALOID / UTAU culture, and language learning.
+I'm a college student in Taiwan, studying, um, Applied English.
+
+I write Emacs Lisp, make covers with UTAU, and occasionally play with Blender.
 
 ◊link["https://gitlab.com/kisaragi-hiu/dotfiles"]{My dotfiles.}
 
-◊subheading[#:id "links"]{Links}
+◊heading[#:id "links"]{Links}
+
+◊subheading{Social}
 
 ◊ul{
 ◊li{◊twitter["flyin1501"]{Twitter}}
+◊li{◊link["https://www.reddit.com/user/flyin1501"]{Reddit}}
+◊li{◊link["https://home.gamer.com.tw/c12345678999 "]{gamer.com.tw}}
+}
+
+◊subheading{Code}
+
+◊ul{
 ◊li{◊github["kisaragi-hiu"]{Github}}
 ◊li{◊gitlab["kisaragi-hiu"]{Gitlab.com}}
 ◊li{◊link["https://git.sr.ht/~kisaragi_hiu/"]{Sourcehut}}
+}
+
+◊subheading{Art}
+
+◊ul{
 ◊li{◊youtube["channel/UCl_hsqcvdX0XdgBimRQ6R3A"]{Youtube}}
 ◊li{◊niconico["user/38995186"]{niconico}}
-◊li{◊link["http://pixiv.me/kisaragi-hiu"]{Pixiv}}
+◊li{◊link["http://pixiv.me/kisaragi-hiu"]{pixiv}}
+◊li{◊link["https://episode.cc/about/flyin1501"]{Episode.cc}}
 }
+
+◊subheading{Gaming}
+
+◊ul{
+◊li{◊link["https://osu.ppy.sh/users/3996811"]{osu!}}
+}
+
+◊heading{About this website}
+
+This site is made with ◊link["https://pollenpub.com/"]{Pollen}, a publishing system based on ◊link["https://racket-lang.org/"]{Racket}.
+
+The icons at the footer are from ◊link["https://feathericons.com/"]{Feather}.
