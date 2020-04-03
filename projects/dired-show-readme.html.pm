32bee632599f052edaf51977e8ac57867e4ea21a
Kisaragi Hiu <mail@kisaragi-hiu.com>
2020-04-04 00:22:28 +0900

    Add image for dired-show-readme

--- projects/dired-show-readme.html.pm
+++ projects/dired-show-readme.html.pm
@@ -6,6 +6,8 @@
 
 ◊gitlab{kisaragi-hiu/dired-show-readme}
 
+◊image["/static/dired-show-readme-0.2.1.png" "dired-show-readme in my dotfiles"]
+
 A Dired extension to show README of a directory like code foundries.
 
 I decided to make this after seeing it in a recent (as of 2020-04) version of Nextcloud.
