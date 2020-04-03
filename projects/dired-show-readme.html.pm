b34cf6ee8c590652b6bf9ffb2c1366a26e1f0be9
Kisaragi Hiu <mail@kisaragi-hiu.com>
2020-04-04 00:05:45 +0900

    Add link to dired-show-readme

--- projects/dired-show-readme.html.pm
+++ projects/dired-show-readme.html.pm
@@ -4,6 +4,8 @@
 ◊define-meta[title]{dired-show-readme}
 ◊heading{dired-show-readme}
 
+◊gitlab{kisaragi-hiu/dired-show-readme}
+
 A Dired extension to show README of a directory like code foundries.
 
 I decided to make this after seeing it in a recent (as of 2020-04) version of Nextcloud.
