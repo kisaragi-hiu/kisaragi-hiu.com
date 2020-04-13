cb8ba6c7bfe424ea4d62d37360e1ecd81154ad73
Kisaragi Hiu <mail@kisaragi-hiu.com>
2020-04-13 21:05:16 +0900

    More "tidy" colors

--- css/main.css.pp
+++ css/main.css.pp
@@ -21,19 +21,22 @@
   `("Overpass Mono" "Noto Sans Mono CJK" ,@cjk-fallback monospace))
 
 ◊(define heading "h1,h2,h3,h4,h5,h6,h7")
-◊(define text-secondary "#555")
+◊(define color-secondary "#555")
+◊(define color-primary "#444")
+◊(define color-accent "#934def")
+◊(define color-accent-strong "#ef4da9")
 
 /*
 * Base */
 
 body {
-    margin: 2em 20%;
-    line-height: 1.6;
-    font-family: ◊&[sans-serif];
-    font-size: 16pt;
-    font-weight: 400;
-    color: #444;
-    text-rendering: optimizeLegibility;
+  margin: 2em 20%;
+  line-height: 1.6;
+  font-family: ◊&[sans-serif];
+  font-size: 16pt;
+  font-weight: 400;
+  color: ◊|color-primary|;
+  text-rendering: optimizeLegibility;
 }
 
 /*
@@ -43,73 +46,73 @@ body {
 ** Prevent transitions from happening during load */
 
 .js-loading {
-    -webkit-transition: none !important;
-    -moz-transition: none !important;
-    -o-transition: none !important;
-    -ms-transition: none !important;
-    transition: none !important;
+  -webkit-transition: none !important;
+  -moz-transition: none !important;
+  -o-transition: none !important;
+  -ms-transition: none !important;
+  transition: none !important;
 }
 
 .disabled {
-    color: ◊|text-secondary|;
+  color: ◊|color-secondary|;
 }
 
 /*
 * Site Header */
 
 #siteheader {
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
 }
 #logo {
-    display: flex;
-    align-items: center;
+  display: flex;
+  align-items: center;
 }
 #logo h1 {
-    margin-top: 0.25rem;
+  margin-top: 0.25rem;
 }
 #logo img {
-    max-height: 2rem;
-    margin-right: 0.4rem;
+  max-height: 2rem;
+  margin-right: 0.4rem;
 }
 #siteheader nav {
-    display: flex;
-    justify-content: flex-end;
+  display: flex;
+  justify-content: flex-end;
 }
 #siteheader nav a {
-    color: #6c757d;
-    margin: 0.5rem 0.8rem 0.5rem 0;
-    font-size: 16pt;
+  color: ◊|color-secondary|;
+  margin: 0.5rem 0.8rem 0.5rem 0;
+  font-size: 16pt;
 }
 
 /*
 * Site footer */
 
 #sitefooter {
-    color: ◊|text-secondary|;
+  color: ◊|color-secondary|;
 }
 
 #sitefooter p {
-    line-height: 1.4;
-    margin: 0.5rem auto 0.5rem;
+  line-height: 1.4;
+  margin: 0.5rem auto 0.5rem;
 }
 
 #footer-sep {
-    margin-top: 2.5rem;
-    margin-bottom: 2.5rem;
-    text-align: center;
-    font-weight: 700;
+  margin-top: 2.5rem;
+  margin-bottom: 2.5rem;
+  text-align: center;
+  font-weight: 700;
 }
 
 #sitefooter nav {
-    display: flex;
-    justify-content: flex-start;
+  display: flex;
+  justify-content: flex-start;
 }
 #sitefooter nav a {
-    color: #6c757d;
-    margin: 0.5rem 1rem 0.5rem 0;
-    font-size: 1.2rem;
+  color: ◊|color-secondary|;
+  margin: 0.5rem 1rem 0.5rem 0;
+  font-size: 1.2rem;
 }
 
 /*
@@ -126,67 +129,80 @@ body {
 /*
 * TOC */
 #toc-title {
-    font-size: 16pt;
+  font-size: 16pt;
 }
 .toc {
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 }
-.toc-h1, .toc-h2 {
-    font-weight: 400;
-    font-size: 16pt;
+.toc-h1,
+.toc-h2 {
+  font-weight: 400;
+  font-size: 16pt;
 }
-.toc-h2 { text-indent: 1rem; }
-.toc-h3 { text-indent: 2rem; }
-.toc-h4 { text-indent: 3rem; }
-.toc-h5 { text-indent: 4rem; }
-.toc-h6 { text-indent: 5rem; }
+.toc-h2 {
+  text-indent: 1rem;
+}
+.toc-h3 {
+  text-indent: 2rem;
+}
+.toc-h4 {
+  text-indent: 3rem;
+}
+.toc-h5 {
+  text-indent: 4rem;
+}
+.toc-h6 {
+  text-indent: 5rem;
+}
 
 /*
 * Builtin tags */
 
-◊; "+" h1,h2 h2,h3 => h1+h2,h2+h2,h1+h3,h2+h3
+/* css-op-all: "+" h1,h2 h2,h3 => h1+h2,h2+h2,h1+h3,h2+h3 */
 ◊(css-op-all "+" heading heading) {
-    margin-top: 1rem;
+  margin-top: 1rem;
 }
 
 ◊|heading| {
-    font-size: 20pt;
-    line-height: 1.2;
-    margin-top: 2rem;
-    margin-bottom: 0.5rem;
-    font-weight: 300;
-    letter-spacing: normal;
+  font-size: 20pt;
+  line-height: 1.2;
+  margin-top: 2rem;
+  margin-bottom: 0.5rem;
+  font-weight: 300;
+  letter-spacing: normal;
 }
 
 h1 {
-    font-weight: 600;
-    color: #444;
+  font-weight: 600;
+  color: ◊|color-primary|;
 }
 
-h2 { font-weight: 400; }
+h2 {
+  font-weight: 400;
+}
 
 blockquote {
-    border-left: 0.3rem solid #ccc;
-    margin: 1rem 0;
-    padding: 0 0.3rem;
-    font-style: italic;
-    background: #f3f3f3;
+  border-left: 0.3rem solid #ccc;
+  margin: 1rem 0;
+  padding: 0 0.3rem;
+  font-style: italic;
+  background: #f3f3f3;
 }
 
 a {
-    color: #a868e8;
-    -webkit-transition: all 0.4s ease;
-    -moz-transition: all 0.4s ease;
-    -o-transition: all 0.4s ease;
-    -ms-transition: all 0.4s ease;
-    transition: all 0.4s ease;
-    text-decoration: none;
+  color: ◊|color-accent|;
+  -webkit-transition: all 0.4s ease;
+  -moz-transition: all 0.4s ease;
+  -o-transition: all 0.4s ease;
+  -ms-transition: all 0.4s ease;
+  transition: all 0.4s ease;
+  text-decoration: none;
 }
 
 a:hover {
-    color: #d0a3ff;
-    text-decoration: none;
+  color: ◊|color-accent-strong|;
+  text-decoration: none;
 }
 
 /*
@@ -195,102 +211,104 @@ a:hover {
   (css-expr
    [|div#references|
     #:margin-top 3rem
-    #:color |#555|
     [p #:margin-top 0 #:margin-bottom 0]]))
+  .div#references {
+  color: #555;
+}
 
 .ref-desc-id {
-    font-family: ◊&[monospace];
+  font-family: ◊&[monospace];
 }
 
 /*
 * Index items */
 .index-year > h1 {
-    margin: 3rem 0 1rem;
+  margin: 3rem 0 1rem;
 }
 
 .index-item {
-    margin-top: 0.5rem;
-    margin-bottom: 1.5rem;
+  margin-top: 0.5rem;
+  margin-bottom: 1.5rem;
 }
 
 .index-item .title {
-    margin: 0;
+  margin: 0;
 }
 
 .index-item .date,
 .index-item .category,
 .index-item .tags {
-    font-family: ◊&[monospace];
-    display: inline-block;
-    color: ◊|text-secondary|;
-    margin: 0 0.5em 0 0;
-    font-size: 16pt;
+  font-family: ◊&[monospace];
+  display: inline-block;
+  color: ◊|color-secondary|;
+  margin: 0 0.5em 0 0;
+  font-size: 16pt;
 }
 
 .index-item .title a {
-    color: #333;
+  color: ◊|color-primary|;
 }
 
 /*
 * Other widgets */
 .badge {
-    margin: auto 0.5rem;
+  margin: auto 0.5rem;
 }
 
 .page-navigation {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    font-size: 16pt;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  font-size: 16pt;
 }
 
 .image p {
-    margin-top: 0;
-    margin-bottom: 0;
+  margin-top: 0;
+  margin-bottom: 0;
 }
 .image-caption {
-    text-align: center;
-    font-family: ◊&[monospace];
-    font-style: italic;
+  text-align: center;
+  font-family: ◊&[monospace];
+  font-style: italic;
 }
 
 .image-link {
-    -webkit-box-shadow: 0px 0px 10px 0px rgba(50, 50, 50, 0.68);
-    -moz-box-shadow:    0px 0px 10px 0px rgba(50, 50, 50, 0.68);
-    box-shadow:         0px 0px 10px 0px rgba(50, 50, 50, 0.68);
+  -webkit-box-shadow: 0px 0px 10px 0px rgba(50, 50, 50, 0.68);
+  -moz-box-shadow: 0px 0px 10px 0px rgba(50, 50, 50, 0.68);
+  box-shadow: 0px 0px 10px 0px rgba(50, 50, 50, 0.68);
 }
 
 .ytembed-default {
-    padding-bottom: 50%;
-    position: relative;
-    overflow: hidden;
+  padding-bottom: 50%;
+  position: relative;
+  overflow: hidden;
 }
 
 /*
 * Code blocks */
 .highlight {
-    font-size: 16pt;
-    overflow-x: auto;
-    font-family: ◊&[monospace];
-    color: black;
+  font-size: 16pt;
+  overflow-x: auto;
+  font-family: ◊&[monospace];
+  color: ◊|color-primary|;
 }
 
 .highlight .linenos {
-    padding-right: 0.3rem;
+  padding-right: 0.3rem;
 }
 
 .linenodiv pre {
-    color: #aaa;
+  color: ◊|color-secondary|;
 }
 
 /*
 * Smaller screen size */
 @media (max-width: 1200px) {
-    body {
-        margin: 0 5%;
-    }
+  body {
+    margin: 0 5%;
+  }
 }
 
-◊; Local Variables:
-◊; eval: (outline-minor-mode)
-◊; End:
+/* Local Variables: */
+/* eval: (outline-minor-mode) */
+/* End: */
