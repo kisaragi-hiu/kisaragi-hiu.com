cd25f7d4f8fd6582ef1d91669b6f966d81ea0d33
Kisaragi Hiu <mail@kisaragi-hiu.com>
2020-04-13 21:05:34 +0900

    tidier project links on index

--- index.html.pm
+++ index.html.pm
@@ -7,8 +7,8 @@ Just a random person on the internet.
 Stuff I've made:
 
 ◊ul{
-◊li{◊link["https://gitlab.com/canrylog/canrylog.el"]{Canrylog (WIP time tracking app)}}
-◊li{◊link["https://barren-moon.kisaragi-hiu.com"]{A Barren Land of Dust (Poem collection from my high school years)}}
+◊li{◊link["https://gitlab.com/canrylog/canrylog.el"]{Canrylog} (WIP time tracking app)}
+◊li{◊link["https://barren-moon.kisaragi-hiu.com"]{A Barren Land of Dust} (Poem collection from my high school years)}
 ◊li{UTAU Covers (◊youtube["channel/UCl_hsqcvdX0XdgBimRQ6R3A"]{on Youtube}, ◊link["https://www.nicovideo.jp/user/38995186"]{on niconico})}
 }
 
