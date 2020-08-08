3f6786697eba3720bfef15fcdd659939473bc888
Kisaragi Hiu <mail@kisaragi-hiu.com>
2020-08-08 23:09:26 +0900

    Set org-roam-directory locally
    (Can't we just use "."?)

--- new/.dir-locals.el
+++ new/.dir-locals.el
@@ -2,10 +2,15 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((org-mode
+  (eval setq org-roam-directory
+        (f-join
+         (projectile-project-root)
+         "new"))
   (org-link-abbrev-alist
    ("youtube" . "https://youtube.com/")
    ("niconico" . "https://nicovideo.jp/")
    ("twitter" . "https://twitter.com/")
    ("github" . "https://github.com/")
    ("gitlab" . "https://gitlab.com/"))))
+
 
