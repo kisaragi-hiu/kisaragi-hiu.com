5896c44082c4bdeee908bc7453f604333e8b38b0
Kisaragi Hiu <mail@kisaragi-hiu.com>
2020-10-21 03:47:22 +0900

    Actual fix: pmd files should now show

--- rkt/pagetree.rkt
+++ rkt/pagetree.rkt
@@ -5,34 +5,41 @@
 
 (provide (all-defined-out))
 
-;; Create a pagetree leaf of html.pm files under DIRECTORY.
+;; Create a pagetree leaf for each pollen source file under DIRECTORY.
 ;; NODE is the leaf's tag.
 ;;
-;; e.g. we have a.html.pm and b.html.pm under projects/;
+;; e.g. we have a.html.pm and b.html.pmd under projects/;
 ;; calling `leaf-from-dir 'xyz "projects"` returns
 ;; '(xyz projects/a.html projects/b.html)
 ;;
 ;; Use eg. (leaf-from-dir 'node "dir" #:suffix ".xml.pm") to list
-;; non-html files.
+;; specifically XML files.
 ;;
 ;; pass a compare function to #:sort to sort the entries before
 ;; inserting them into the pagetree. The function is passed straight
-;; to `sort`: it gets two arguments A and B, and returns non-nil if A
-;; sorts before B. By default it applies string>?, taking care to
-;; convert symbols to strings.
+;; to `sort`: it gets two string arguments A and B, and should return
+;; non-nil if A sorts before B.
 (define (leaf-from-dir node directory
                        #:suffix [suffix #f]
                        #:sort [sort-func
-                               (lambda args
-                                 (apply string>? (map symbol->string args)))])
+                               string>?])
   (~>> (directory-list directory #:build? #t)
        (map path->string)
+       (filter
+        (lambda (val)
+          (or
+           (markup-source? val)
+           (markdown-source? val)
+           (preproc-source? val)
+           (null-source? val)
+           (scribble-source? val)
+           (pagetree-source? val))))
        (filter
         (if suffix
             (curryr string-suffix? suffix)
             identity))
        (map ->output-path)
        (map path->string)
-       (map string->symbol)
        (sort _ sort-func)
+       (map string->symbol)
        (cons node)))
