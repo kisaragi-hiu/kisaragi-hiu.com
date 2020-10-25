72dad0a73e8561be97029b0e200fc424f3a5cf0b
Kisaragi Hiu <mail@kisaragi-hiu.com>
2020-10-25 18:58:43 +0900

    wording

--- projects/ox-pollen.html.pm
+++ projects/ox-pollen.html.pm
@@ -8,6 +8,8 @@ ox-pollen allows exporting Org mode to Pollen markup.
 
 The Pollen markup it generates is somewhat opinionated, simply because of how flexible Pollen markup is.
 
-It is to convert Org mode keywords (◊code{#+title:} and the like) to ◊code{define-meta} statements. Combined with Make, it makes for a pretty nice workflow for writing pages in Org in a Pollen project.
+It supports converting Org mode keywords (◊code{#+title:} and the like) to ◊code{define-meta} statements.
+
+Combined with Make, it makes for a pretty nice workflow for writing pages in Org in a Pollen project.
 
 Repository: ◊link["https://github.com/kisaragi-hiu/ox-pollen"]
