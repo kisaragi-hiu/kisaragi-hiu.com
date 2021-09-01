ffc2180185e39697b7a5bb6ef5e2c9bd938f043f
Kisaragi Hiu <mail@kisaragi-hiu.com>
2021-09-02 03:29:51 +0900

    move a bunch more css to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,21 +24,6 @@ module.exports = {
         "monospace",
       ],
     },
-    fontSize: {
-      xs: ["0.75rem", { lineHeight: "1rem" }],
-      sm: ["12pt", { lineHeight: "1.25rem" }],
-      base: ["14pt", { lineHeight: "1.6" }],
-      lg: ["16pt", { lineHeight: "1.75rem" }],
-      xl: ["24pt", { lineHeight: "1.6" }],
-      "2xl": ["36pt", { lineHeight: "2" }],
-      "3xl": ["1.875rem", { lineHeight: "2.25rem" }],
-      "4xl": ["2.25rem", { lineHeight: "2.5rem" }],
-      "5xl": ["3rem", { lineHeight: "1" }],
-      "6xl": ["3.75rem", { lineHeight: "1" }],
-      "7xl": ["4.5rem", { lineHeight: "1" }],
-      "8xl": ["6rem", { lineHeight: "1" }],
-      "9xl": ["8rem", { lineHeight: "1" }],
-    },
     extend: {
       // https://github.com/tailwindlabs/tailwindcss/discussions/1361
       colors: {
