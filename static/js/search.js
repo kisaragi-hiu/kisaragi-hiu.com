af503d6c28178d8cea44db1364aa37f73e1e1cfe
Kisaragi Hiu <mail@kisaragi-hiu.com>
2022-02-01 00:12:06 +0900

    search should match series and tags as well

--- static/js/search.js
+++ static/js/search.js
@@ -1,9 +1,19 @@
+function shouldInclude(params, needle) {
+  let title = params.title || "";
+  title = title.toLowerCase();
+  let series = params.series || "";
+  series = series.toLowerCase();
+  let tags = params.tags || [""];
+  tags = tags.join("").toLowerCase();
+  return (title + tags + series).includes(needle);
+}
+
 function updateSearch() {
   let input_elem = document.getElementById("search");
   let needle = input_elem.value.toLowerCase();
   for (const post of document.getElementById("posts").children) {
     let params = JSON.parse(post.getAttribute("data-params"));
-    if (params.title.toLowerCase().includes(needle)) {
+    if (shouldInclude(params, needle)) {
       post.style.display = "unset";
     } else {
       post.style.display = "none";
