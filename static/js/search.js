2f6e8eeaa2a9c805c19a27d91e8ba0e616332363
Kisaragi Hiu <mail@kisaragi-hiu.com>
2022-04-02 23:33:03 +0900

    Add a language filter

--- static/js/search.js
+++ static/js/search.js
@@ -13,7 +13,14 @@ function ensureArray(obj) {
   }
 }
 
-function shouldInclude(params, needle) {
+function shouldInclude(params, needle, filters) {
+  if (
+    !filters.every((filter) => {
+      return params[filter.type] == filter.value;
+    })
+  ) {
+    return false;
+  }
   let title = ensureString(params.title);
   let series = ensureString(params.series);
   let voice = ensureString(params.voice);
@@ -26,13 +33,19 @@ function shouldInclude(params, needle) {
   // "a b" -> only items including both "a" and "b" are included
   return needle.split(" ").every((x) => str.includes(x));
 }
-
 function updateSearch() {
   let input_elem = document.getElementById("search");
+  let filters = [];
+  document
+    .getElementById("filters")
+    .querySelectorAll("input:checked")
+    .forEach((x) => {
+      filters.push(JSON.parse(x.getAttribute("data-filter")));
+    });
   let needle = input_elem.value.toLowerCase();
   for (const post of document.getElementById("posts").children) {
     let params = JSON.parse(post.getAttribute("data-params"));
-    if (shouldInclude(params, needle)) {
+    if (shouldInclude(params, needle, filters)) {
       post.style.display = "unset";
     } else {
       post.style.display = "none";
