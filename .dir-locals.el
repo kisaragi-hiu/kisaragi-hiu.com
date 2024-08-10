838eded7d2a5e463dc88afb8cf9802c93c373720
Kisaragi Hiu <mail@kisaragi-hiu.com>
2024-08-10 12:48:28 +0900

    2024-07

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
-;;; Directory Local Variables
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((org-mode . ((minaduki:link-insertion-format . relative))))
+((org-mode . ((eval . (setq org-attach-id-dir (expand-file-name "static/" (project-root (project-current)))))
+              (minaduki:link-insertion-format . relative))))
